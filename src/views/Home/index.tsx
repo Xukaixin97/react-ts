f0942ff8ea3a40b510d85941a75f6108fff70ee4
xuminda <xuminda@hikwuxi.com>
2024-10-30 10:18:34 +0800

    feat: 地图显示

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -11,36 +11,40 @@ import VehicleCity from './components/VehicleCity'
 import VehicleType from './components/VehicleType'
 
 function Home() {
+  const [onlyMap] = useState(false)
   return (
 
     <div className="w-full h-screen relative">
       <BMAP />
 
-      <header className="w-full absolute top-0 left-0">
-        <Title />
-        <CurrentTime />
-      </header>
+      {onlyMap && (
+        <>
+          <header className="w-full absolute top-0 left-0">
+            <Title />
+            <CurrentTime />
+          </header>
 
-      <section about="left panel" className="flex flex-col gap-4 justify-between w-auto  absolute top-[6.75rem] left-6 z-50 h-[calc(100vh-8.25rem)] ">
-        <DeviceStatus />
-        <ActiveUsers />
-        <NewData />
-      </section>
+          <section about="left panel" className="flex flex-col gap-4 justify-between w-auto  absolute top-[6.75rem] left-6 z-50 h-[calc(100vh-8.25rem)] ">
+            <DeviceStatus />
+            <ActiveUsers />
+            <NewData />
+          </section>
 
-      <section about="right panel" className="flex flex-col gap-4 justify-between w-auto  absolute top-[6.75rem] right-6 z-50 h-[calc(100vh-8.5rem)] ">
-        <VehicleType />
-        <VehicleCity />
-        <SalesChannel />
-      </section>
+          <section about="right panel" className="flex flex-col gap-4 justify-between w-auto  absolute top-[6.75rem] right-6 z-50 h-[calc(100vh-8.5rem)] ">
+            <VehicleType />
+            <VehicleCity />
+            <SalesChannel />
+          </section>
 
-      <section about="TotalSummary" className="w-[calc(100%-47.75rem)]   absolute top-[7.75rem]  left-[50%] translate-x-[-50%] z-50 ">
-        <TotalSummary />
-      </section>
+          <section about="TotalSummary" className="w-[calc(100%-47.75rem)]   absolute top-[7.75rem]  left-[50%] translate-x-[-50%] z-50 ">
+            <TotalSummary />
+          </section>
 
-      <section about="AlarmList" className="w-[calc(100%-52rem)]   absolute  bottom-6  left-[50%] translate-x-[-50%] z-50 ">
-        <AlarmList />
-      </section>
-
+          <section about="AlarmList" className="w-[calc(100%-52rem)]   absolute  bottom-6  left-[50%] translate-x-[-50%] z-50 ">
+            <AlarmList />
+          </section>
+        </>
+      )}
     </div>
   )
 }
